99f1fa38fcf81649e0cf54d12274b911b737e2ad
Prakhar <prakhar2788@gmail.com>
2024-02-07 14:16:30 +0530

    [Prakhar] Add: UC2- Ability to add new contacts in addressbook

--- AddressBook.cs
+++ AddressBook.cs
@@ -14,6 +14,66 @@ namespace Address_Book
         public AddressBook()
         {
             contactslist = new List<Contact>();
-        }   
+        }
+
+
+        public bool DoesEmailExist(string email)
+        {
+            return contactslist.Any(contact => string.Equals(contact.Email, email, StringComparison.OrdinalIgnoreCase));
+        }
+
+        internal void AddContact()
+        {
+            /*if (DoesEmailExist(email))
+            {
+                Console.WriteLine("A contact with the same email already exists. Contact not added.");
+                return;
+            }*/
+
+            // User input details and send the details through constructor
+
+
+            Contact newContact = new Contact();
+
+
+            Console.WriteLine("Write email");
+            string email = Console.ReadLine();
+            if (DoesEmailExist(email))
+            {
+                Console.WriteLine("Email already exist! Enter again...");
+                Console.WriteLine("Press Enter to Exit");
+                Console.ReadLine();
+                return;
+            }
+            Console.WriteLine("first name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("write Phone Number ");
+            string phoneNumber = Console.ReadLine();
+            Console.WriteLine("Write Pincode ");
+            string zip = Console.ReadLine();
+            Console.WriteLine("Write address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Write City ");
+            string city = Console.ReadLine();
+            Console.WriteLine("Write Country ");
+            string country = Console.ReadLine();
+            Console.WriteLine("Write State");
+            string state = Console.ReadLine();
+
+            newContact.FirstName = firstName;
+            newContact.LastName = lastName;
+            newContact.Address = address;
+            newContact.City = city;
+            newContact.State = state;
+            newContact.Country = country;
+            newContact.Zip = zip;
+            newContact.PhoneNumber = phoneNumber;
+            newContact.Email = email;
+
+            contactslist.Add(newContact);
+            Console.WriteLine("Contact added successfully!");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -17,8 +17,30 @@ namespace Address_Book
             AddressBook addressBook = new AddressBook();
 
             Console.WriteLine(" Welcome to AddressBookMenu:");
-        }
+            Console.WriteLine("1. Add New Contact");
 
-        
+            Console.WriteLine("2. Exit");
+
+            Console.Write("Enter your choice (1-2): ");
+            string userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "1":
+                    // Add a new contact
+                    addressBook.AddContact();
+                    break;
+
+                case "2":
+                    // Exit the program
+                    Console.WriteLine("Exiting the program. Goodbye!");
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please enter a valid option (1-5).");
+                    break;
+            }
+
+        }
     }
 }
